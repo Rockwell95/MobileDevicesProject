45deb126c08aff0c69c7c5231ce3f7da8ab9949b
Nephna <ilunamist@gmail.com>
2016-11-13 19:28:04 -0500

    Nearly complete with SignupActivity

--- app/src/main/java/com/example/devin/mobiledevicesproject/SignupActivity.java
+++ app/src/main/java/com/example/devin/mobiledevicesproject/SignupActivity.java
@@ -12,18 +12,29 @@ public class SignupActivity extends AppCompatActivity {
     EditText editUserEmail;
     EditText editUserPassword;
     EditText editUserPasswordConfirm;
+    EditText editUserBirthdateDay;
+    EditText editUserBirthdateMonth;
+    EditText editUserBirthdateYear;
+    boolean[] errors;
 
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
 
+        // 0 = email, 1 = password, 2 = passwordConfirm
+        errors = new boolean[6];
+
         editUserFirstName = (EditText) findViewById(R.id.editUserFirstName);
         editUserLastName = (EditText) findViewById(R.id.editUserLastName);
         editUserEmail = (EditText) findViewById(R.id.editUserEmail);
         editUserPassword = (EditText) findViewById(R.id.editUserPassword);
         editUserPasswordConfirm = (EditText) findViewById(R.id.editUserPasswordConfirm);
+        editUserBirthdateDay = (EditText) findViewById(R.id.editUserBirthdateDay);
+        editUserBirthdateMonth = (EditText) findViewById(R.id.editUserBirthdateMonth);
+        editUserBirthdateYear = (EditText) findViewById(R.id.editUserBirthdateYear);
 
+        // validates email
         editUserEmail.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
@@ -31,13 +42,16 @@ public class SignupActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override
             public void afterTextChanged(Editable editable) {
-                // TODO: add proper input authentication
-                if (!editUserEmail.getText().toString().matches("@+")) {
-                    editUserEmail.setError("Invalid email");
+                if (!editUserEmail.getText().toString().matches("^\\w+@\\w+\\.\\w+$")) {
+                    editUserEmail.setError(getString(R.string.errorEmail));
+                    errors[0] = true;
+                } else {
+                    errors[0] = false;
                 }
             }
         });
 
+        // validates initial password
         editUserPassword.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
@@ -45,7 +59,82 @@ public class SignupActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override
             public void afterTextChanged(Editable editable) {
+                String passwordRegex = "^(?=.*[a-z]+)(?=.*[A-Z])(?=.*\\d).{8,}$";
 
+                if (!editUserPassword.getText().toString().matches(passwordRegex)) {
+                    editUserPassword.setError(getString(R.string.errorPasswordInvalid));
+                    errors[1] = true;
+                } else {
+                    errors[1] = false;
+                }
+            }
+        });
+
+        // validates confirmed password
+        editUserPasswordConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editUserPasswordConfirm.getText().toString().equals(editUserPassword.getText().toString())) {
+                    editUserPasswordConfirm.setError(getString(R.string.errorMatchingPassword));
+                    errors[2] = true;
+                } else {
+                    errors[2] = false;
+                }
+            }
+        });
+
+        // validates birth day
+        editUserBirthdateDay.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editUserBirthdateDay.getText().toString().matches("^[\\d]{1,2}$")) {
+                    editUserBirthdateDay.setError(getString(R.string.errorBirthdateDay));
+                    errors[3] = true;
+                } else {
+                    errors[3] = false;
+                }
+            }
+        });
+
+        // validates month
+        editUserBirthdateMonth.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editUserBirthdateMonth.getText().toString().matches("^[\\d]{1,2}$")) {
+                    editUserBirthdateMonth.setError(getString(R.string.errorBirthdateMonth));
+                    errors[4] = true;
+                } else {
+                    errors[4] = false;
+                }
+            }
+        });
+
+        // validates birth year
+        editUserBirthdateYear.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editUserBirthdateYear.getText().toString().matches("^[\\d]{4}$")) {
+                    editUserBirthdateYear.setError(getString(R.string.errorBirthdateYear));
+                    errors[5] = true;
+                } else {
+                    errors[5] = false;
+                }
             }
         });
     }
